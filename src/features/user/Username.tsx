fc4dbe6712e45534b750f97646ea6541e39426fa
acharayaP03 <acharyaP03@gmail.com>
2024-09-18 17:29:03 +1000

    tailwind typography

---
+++ src/features/user/Username.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Username() {
+  return (
+    <div className="hidden text-sm font-semibold">
+      <p>Trishten ac</p>
+    </div>
+  );
+}
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Fast Pizza & Co.</Link>
+    <header className="border-b border-stone-300 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-[5px]">
+        Fast Pizza & Co.
+      </Link>
       <SearchOrder />
 
-      <p>Trishten ac</p>
+      <Username />
     </header>
   );
 }
