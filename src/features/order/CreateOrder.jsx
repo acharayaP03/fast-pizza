8d592d1ff11a9e326bb274a39b1ec0cfaeebf999
acharayaP03 <acharyaP03@gmail.com>
2024-09-18 18:08:40 +1000

    buttons and links

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -78,7 +78,10 @@ function CreateOrder() {
         </div>
 
         <div>
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:bg-yellow-500 disabled:cursor-not-allowed"
+          >
             {isSubmitting ? 'Placing order..' : 'Order now'}
           </button>
         </div>
