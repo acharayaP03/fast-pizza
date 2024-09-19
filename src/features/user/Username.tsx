6dd581abe71d3bcc4f34ebfdafc825b3bc3a4480
acharayaP03 <acharyaP03@gmail.com>
2024-09-19 10:43:56 +1000

    install and configure redux

--- src/features/user/Username.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Username() {
-  return (
-    <div className="hidden text-sm font-semibold md:block">
-      <p>Trishten ac</p>
-    </div>
-  );
-}
