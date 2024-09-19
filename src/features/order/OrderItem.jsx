407d7b017d02f90c02d8e26f0e8bb30fa2f64621
acharayaP03 <acharyaP03@gmail.com>
2024-09-19 10:23:52 +1000

    style order items

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,29 +1,29 @@
-import PropTypes from "prop-types";
-import { formatCurrency } from "../../utils/helpers";
+import PropTypes from 'prop-types';
+import { formatCurrency } from '../../utils/helpers';
 
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li>
-      <div>
+    <li className="py-3">
+      <div className="flex items-center justify-between text-sm">
         <p>
-          <span>{quantity}&times;</span> {name}
+          <span className="font-bold">{quantity}&times;</span> {name}
         </p>
-        <p>{formatCurrency(totalPrice)}</p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
     </li>
   );
 }
 
-OrderItem.proptypes = {
-  item: PropTypes.shape({
-    quantity: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    totalPrice: PropTypes.number.isRequired,
-  }).isRequired,
-  isLoadingIngredients: PropTypes.bool.isRequired,
-  ingredients: PropTypes.arrayOf(PropTypes.string),
-};
+// OrderItem.proptypes = {
+//   item: PropTypes.shape({
+//     quantity: PropTypes.number.isRequired,
+//     name: PropTypes.string.isRequired,
+//     totalPrice: PropTypes.number.isRequired,
+//   }).isRequired,
+//   isLoadingIngredients: PropTypes.bool.isRequired,
+//   ingredients: PropTypes.arrayOf(PropTypes.string),
+// };
 
 export default OrderItem;
